0aff68dfed94dd4c5cd644e64fbc5d681436b2fc
ArslanYM <arsalanyaqmalik@gmail.com>
2023-02-21 22:02:54 +0530

    added server files and dependencies

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey:process.env.OPENAI_API_KEY,
+}) 
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors);
+app.use(express.json());
+
+
+app.get('/',async(req,res)=>{
+res.status(200).send({
+    message:'Hello from CodeIt',
+})
+})
+
+app.post('/',async(req,res)=>{
+    try {
+    const prompt = req.body.prompt;
+    const response =await openai.createCompletion({
+        //work tomo
+    })
+    } catch {
+
+    }
+})
