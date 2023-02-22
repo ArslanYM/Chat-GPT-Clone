31f06dcb311bb6bc7ab0afb1da53670f28b0c50e
ArslanYM <arsalanyaqmalik@gmail.com>
2023-02-22 21:45:20 +0530

    working app with server

--- client/script.js
+++ client/script.js
@@ -48,7 +48,7 @@ function chatStripe(isAi, value , uniqueId) {
     `
     <div class="wrapper ${isAi && 'ai'}>
       <div class="chat">
-        <div className="profile">
+        <div class="profile">
         <img
         src="${isAi? bot : user}"
         alt="${isAi? 'bot' : 'user'}"
@@ -81,7 +81,30 @@ const handleSubmit = async(e)=>{
     const messageDiv = document.getElementById(uniqueId);
 
     loader(messageDiv);
+   //fetch data from server -> bots response 
 
+   const response = await fetch('http://localhost:3000',{
+    method:'POST',
+    headers:{
+        'Content-Type':'application/json'
+    },
+    body:JSON.stringify({
+        prompt:data.get('prompt')
+    })
+   })
+   clearInterval(loadInterval);
+   messageDiv.innerHTML='';
+   
+   if(response.ok) {
+    const data =await response.json();
+    const parseData = data.bot.trim();
+
+    typeText(messageDiv,parseData);
+   }else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong!";
+   }
 }
 
 
